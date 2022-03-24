d22c623b0f61d24ddcad7e150a087aac219a433f
rayaalmudena <raya.almudena@gmail.com>
2022-03-24 10:13:31 +0100

    done

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 // Happy coding!
 
+
+document.querySelector(".canvas").addEventListener('click',function(event){
+    console.log(event.target.id);
+    //document.body.style.backgroundColor=event.target.id; 
+    document.body.style.backgroundColor = window.getComputedStyle(event.target).backgroundColor;
+});
